a09f7948f4c0c0a912ccb9890ca04705b8530f24
Alec Minchington <alec.minch@gmail.com>
2020-08-07 00:31:26 -0400

    Attempt to fix serviceworker registration failing

--- js/serviceWorker.js
+++ js/serviceWorker.js
@@ -25,6 +25,8 @@ export function register(config) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
+      console.log(publicUrl.origin, window.location.origin)
+
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebook/create-react-app/issues/2374
@@ -37,7 +39,6 @@ export function register(config) {
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
-
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,41 @@
+const webpack = require("webpack");
 const path = require("path");
-const TerserPlugin = require('terser-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = {
-  mode: "production",
-  entry: "./js/dome.js",
-  output: {
-    path: __dirname + '/static',
-    filename: 'lib.js',
-    libraryTarget: 'var',
-    library: 'Dome'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
+module.exports = function (options) {
+  const plugins = [];
+  plugins.push(
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production"),
+        PUBLIC_URL: JSON.stringify("https://start.alecminch.dev"),
+      },
+    })
+  );
+
+  return {
+    mode: "production",
+    entry: "./js/dome.js",
+    output: {
+      path: __dirname + "/static",
+      filename: "lib.js",
+      libraryTarget: "var",
+      library: "Dome",
+    },
+    plugins: plugins,
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
           },
         },
-      },
-    ],
-  },
+      ],
+    },
+  };
 };
