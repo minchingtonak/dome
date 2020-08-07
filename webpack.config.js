e6ee382eb95e6b7469cd10b2088b4360aafef601
Alec Minchington <alec.minch@gmail.com>
2020-08-07 01:34:24 -0400

    Another attempt at caching with serviceworker

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,50 @@
 const webpack = require("webpack");
 const path = require("path");
-const TerserPlugin = require("terser-webpack-plugin");
+const workbox = require("workbox-webpack-plugin");
 
-module.exports = function (options) {
-  const plugins = [];
-  plugins.push(
+module.exports = {
+  mode: "production",
+  entry: "./js/dome.js",
+  output: {
+    path: __dirname + "/static",
+    filename: "lib.js",
+    libraryTarget: "var",
+    library: "Dome",
+  },
+  plugins: [
     new webpack.DefinePlugin({
       "process.env": {
         NODE_ENV: JSON.stringify("production"),
         PUBLIC_URL: JSON.stringify("https://start.alecminch.dev"),
       },
-    })
-  );
-
-  return {
-    mode: "production",
-    entry: "./js/dome.js",
-    output: {
-      path: __dirname + "/static",
-      filename: "lib.js",
-      libraryTarget: "var",
-      library: "Dome",
-    },
-    plugins: plugins,
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env"],
-            },
+    }),
+    new workbox.GenerateSW({
+      clientsClaim: true,
+      exclude: [/\.map$/, /asset-manifest\.json$/],
+      navigateFallback: "https://start.alecminch.dev/index.html",
+      navigateFallbackDenylist: [
+        // Exclude URLs starting with /_, as they're likely an API call
+        new RegExp("^/_"),
+        // Exclude any URLs whose last part seems to be a file extension
+        // as they're likely a resource and not a SPA route.
+        // URLs containing a "?" character won't be blacklisted as they're likely
+        // a route with query params (e.g. auth callbacks).
+        new RegExp("/[^/?]+\\.[^/]+$"),
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
           },
         },
-      ],
-    },
-  };
+      },
+    ],
+  },
 };
