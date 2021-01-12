fac22bd48df8b06ee48972e1e71b87c4a0172f93
minchingtonak <minchingtonak@users.noreply.github.com>
2021-01-12 18:52:20 +0000

    deploy: 6e4e0ca0323c04af14a5fadaaa2b4c720c132e66

---
+++ precache-manifest.e6b03a33e26dd8d1bf8419e1d88bb739.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e324fbc29c9cb75fbb3046ed08643bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bce4864f58f77a2edfdb",
+    "url": "/static/js/main.93cb61f2.chunk.js"
+  },
+  {
+    "revision": "034193b9e3e06d059911",
+    "url": "/static/js/runtime-main.997d2277.js"
+  }
+]);
